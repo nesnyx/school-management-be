94bbe90ccea1e360e18cc77372883323155c99c3
Aris Setiawan Bara <baraaris2@gmail.com>
2026-01-12 10:23:39 +0000

    feat : add fix fees tuition and payment gateway services

--- src/modules/finance/payment-gateway/payment-gateway.module.ts
+++ src/modules/finance/payment-gateway/payment-gateway.module.ts
@@ -3,10 +3,10 @@ import { PaymentGatewayService } from './payment-gateway.service';
 import { PaymentGatewayController } from './payment-gateway.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeesTuition } from '../fees-tuition/entities/fees-tuition.entity';
-import { EventEmitterModule } from '@nestjs/event-emitter';
+
 import { PaymentGateway } from './entities/payment-gateway.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([FeesTuition, PaymentGateway]), EventEmitterModule.forRoot()],
+  imports: [TypeOrmModule.forFeature([FeesTuition, PaymentGateway])],
   controllers: [PaymentGatewayController],
   providers: [PaymentGatewayService],
   exports: [PaymentGatewayService]
